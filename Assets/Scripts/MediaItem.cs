09270741a4acb6bd644af0b71668d4a86a701c36
a <a@192.168.0.103>
2018-12-14 00:29:17 +0800

    123

---
+++ Assets/Scripts/MediaItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediaItem : MonoBehaviour {
+
+	public SpriteRenderer srAdpicture;
+
+}
---
+++ Assets/Scripts/MediaItemInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediaItemInfo 
+{
+	/// <summary>
+	/// 媒体类型
+	/// </summary>
+	public int iMediaType = 0;
+	/// <summary>
+	/// 媒体类型名称
+	/// </summary>
+	public string sMediaTypeName = "";
+	/// <summary>
+	/// 媒体编号
+	/// </summary>
+	public string sMediaNumber = "";
+	/// <summary>
+	/// 媒体资产编号
+	/// </summary>
+	public string sMediaAssetNumber = "";
+	/// <summary>
+	/// 媒体尺寸
+	/// </summary>
+	public string sMediaSize = "";
+	/// <summary>
+	/// 媒体位置描述
+	/// </summary>
+	public string sMediaLocation = "";
+	/// <summary>
+	/// 媒体广告图片地址
+	/// </summary>
+	public string sMediaPicLink = "";
+	/// <summary>
+	/// 媒体的点位
+	/// </summary>
+	public Vector3 v3MediaPosition = "";
+	/// <summary>
+	/// 媒体的旋转角
+	/// </summary>
+	public Vector3 v3MediaRotation = "";
+
+
+}
